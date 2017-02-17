94db9c51a9264e7fff69cd477469075b84ab5c5b
Namun Cho <namun.cho@gmail.com>
2017-02-17 18:47:36 +0900

    ult1 coding

--- ult1/models.py
+++ ult1/models.py
@@ -41,11 +41,10 @@ class Constants(BaseConstants):
 
 class Subsession(BaseSubsession):
 
-"""
-utl1에서는 취향 질문에 따라 역할을 배정한다. 질문은 binary choice q1, q2
-일단 q1에 기반하여 매칭한 뒤, 남는 사람들을 q2로 매칭하고 그래도 남는 사람들은
-임의배정하는 알고리즘을 사용하기로 함.
-"""
+# utl1에서는 취향 질문에 따라 역할을 배정한다. 질문은 binary choice q1, q2
+# 일단 q1에 기반하여 매칭한 뒤, 남는 사람들을 q2로 매칭하고 그래도 남는 사람들은
+# 임의배정하는 알고리즘을 사용하기로 함.
+
 
     def before_session_starts(self):
         rnd_num = random.randint(1,2)  #이 값이 q1- 1이 맡게 될 group id임.
@@ -83,28 +82,72 @@ utl1에서는 취향 질문에 따라 역할을 배정한다. 질문은 binary c
 
             # 이 과정을 거치면 둘 중 더 많은 놈이 남는다. 따라서 q2에 기반하여 동일한 내용을 반복한다.
 
+            rnd_num = random.randint(1,2)
+
             q2_1_players = [p for p in largerGroup if p.participant.vars['q2'] == True]
             q2_2_players = [p for p in largerGroup if p.participant.vars['q2'] == False]
 
-            
+            count1 = len(q2_1_players)
+            count2 = len(q2_2_players)
 
+            if count1 < count2:
+                smallerGroup = q2_1_players
+                largerGroup = q2_2_players
+            else:
+                smallerGroup = q2_2_players
+                largerGroup = q2_1_players
 
+            while smallerGroup:
+                if rnd_num == 1:
+                    new_group = [
+                        q2_1_players.pop(),
+                        q2_2_players.pop()
+                    ]
+                else:
+                    new_group = [
+                        q2_2_players.pop(),
+                        q2_1_players.pop()
+                    ]
+                group_matrix.append(new_group)
 
+                # 이러고도 남는 아이들은 어쩔 수 없이 랜덤으로 배치한다.
+            while largerGroup:
+                if len(largerGroup) > 1:
+                    new_group = [
+                        largerGroup.pop(),
+                        largerGroup.pop()
+                    ]
+                # 만일 한 명만 남았다면 이 사람은 예외 처리를 해야 한다. 이것을 처리하기 위해서는 크게 두 가지 방식이 있다. (1) 게임 밖에서 처리하는 방식. 즉, 이때는 참가자가 홀수일 경우 추첨을 해서 한 사람 빼고 나머지 짝수 인원으로만 하게 해서 절대 이런 상황이 나오지 않게 하는 것인데, 경험상 좋지 않다. (2) 예외처리를 하는 방법. 코딩이 복잡해지는데, 예외 플래그를 만들어서 체크하는 수 밖에 없을 듯하다.
+                elif len(largerGroup) == 1:
+                    largerGroup.pop().is_exception = True
+                group_matrix.append(new_group)
 
+            self.set_group_matrix(group_matrix)
+        else:
+            self.group_like_round(1)
 
 
 
 class Group(BaseGroup):
-    pass
+    amount_offered = models.CurrencyField(choices=Constants.offer_choices)
+
+    offer_accepted = models.BooleanField()
+
+    def set_payoffs(self): # 1명 그룹이 있을 경우에는 이 부분을 통과할 수 없어야 한다. view 차원에서 아예 블로킹 되어 있어야 함.
+        p1, p2 = self.get_players()
+        if self.offer_accepted:
+            p1.payoff = Constants.endowment - self.amount_offered
+            p2.payoff = self.amount_offered
+        else:
+            p1.payoff = Constants.payoff_if_rejected
+            p2.payoff = Constants.payoff_if_rejected
 
 
 class Player(BasePlayer):
-    proposed_amount = models.CurrencyField(min=0,max=Constants.endowment)
-
-    accept_reject = models.BooleanField() # true:accept, false:reject
+    is_exception = models.BooleanField(default=False) # 사람 수가 맞는지를 체크하기 위한 것. True이면 이 사람은 조 배정에서 남는 사람으로, 이 사람에 대한 처리를 항상 해주어야 한다.
 
     def role(self):
         if self.id_in_group == 1:
-            return 'Proposer'
+            return '제안자'
         if self.id_in_group == 2:
-            return 'Responder'
+            return '응답자'
--- ult1/views.py
+++ ult1/views.py
@@ -4,9 +4,29 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
+class Introduction(Page):
     pass
 
+class ExceptionPage(Page):
+    def is_displayed(self):
+        return self.player.is_exception == True
+
+class Offer(Page):
+    form_model = models.Group
+    form_fields = ['amount_offered']
+
+    def is_displayed(self):
+        return (self.player.id_in_group == 1 and not(self.player.is_exception))
+
+class WaitForProposer(WaitPage):
+    pass
+
+class Accept(Page):
+    form_model = models.Group
+    form_fields = ['offer_accepted']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 2 and not self.player.is_exception
 
 class ResultsWaitPage(WaitPage):
 
@@ -19,7 +39,11 @@ class Results(Page):
 
 
 page_sequence = [
-    MyPage,
+    Introduction,
+    ExceptionPage,
+    Offer,
+    WaitForProposer,
+    Accept,
     ResultsWaitPage,
     Results
 ]
